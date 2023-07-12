a180944a8ff3a23bec1549aa26717e0c4d067775
Isaac Marovitz <isaacryu@icloud.com>
2023-07-12 17:57:41 +0100

    Fix a few fuck ups

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { PedestrianFamily, ToolBox, GameConsole, Linux } from '@vicons/carbon';
 import { GlobalTheme, darkTheme } from 'naive-ui'
 
 import 'vfonts/FiraSans.css';
@@ -46,9 +47,52 @@ function toggleTheme() {
             <div class="top-main">
               <Hook />
             </div>
-
             <div class="column-content">
-              <Home />
+              <MainCard />
+              <ContentCard>
+                <template #card-icon>
+                  <PedestrianFamily />
+                </template>
+                <template #card-title>
+                  Huge community.
+                </template>
+                Everyone is here! The spirit of RPCS3 lives on.
+              </ContentCard>
+              <ContentCard>
+                <template #card-icon>
+                  <ToolBox />
+                </template>
+                <template #card-title>
+                  Ongoing development.
+                </template>
+                Est. 2016 by DH himself.
+              </ContentCard>
+              <ContentCard>
+                <template #card-icon>
+                  <GameConsole />
+                </template>
+                <template #card-title>
+                  All your favorite titles.
+                </template>
+                <div class="joke-wrapper">
+                  <div class="body-text">
+                    Bloodborne coming Soon™*
+                  </div>
+                  <div class="joke-footnote">
+                    * RPCSX does not provide estimates or compatibility for any title.
+                  </div>
+                </div>
+              </ContentCard>
+              <ContentCard>
+                <template #card-icon>
+                  <Linux />
+                </template>
+                <template #card-title>
+                  Currently Linux only.
+                </template>
+                WSL on Windows is being discussed.
+              </ContentCard>
+              <n-divider />
             </div>
           </div>
           <div id="right-column" class="column">
--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,7 +1,7 @@
 <template>
   <n-card hoverable>
     <template #cover>
-      <img src="./assets/logo-light.png" />
+      <img :src="'./assets/logo-light.png'" />
     </template>
     <template #header>
       <div class="card-header">RPCSX is your PlayStation 4 emulator.</div>
@@ -9,3 +9,9 @@
     <div class="body-text">Use only with lawfully obtained archival copies of PS4 games you physically own.</div>
   </n-card>
 </template>
+
+<style scoped>
+    .n-card {
+        max-width: 300px;
+    }
+</style>
