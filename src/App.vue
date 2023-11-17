34ad029e9005e015d31d482be0b72459767fd3c7
OCDkirby <mangacreeperyt@gmail.com>
2023-11-17 15:28:26 -0600

    remove: unused content

--- src/App.vue
+++ src/App.vue
@@ -119,51 +119,8 @@ const darkThemeOverrides: GlobalThemeOverrides = {
                 </Suspense>
               </div>
 
-              <div class="column-content">
-                <!-- <ContentCard>
-                  <template #card-icon>
-                    <PedestrianFamily />
-                  </template>
-                  <template #card-title>
-                    Huge community.
-                  </template>
-                  Everyone is here! The spirit of RPCS3 lives on.
-                </ContentCard>
-                <ContentCard>
-                  <template #card-icon>
-                    <ToolBox />
-                  </template>
-                  <template #card-title>
-                    Ongoing development.
-                  </template>
-                  Est. 2016 by DH himself.
-                </ContentCard>
-                <ContentCard>
-                  <template #card-icon>
-                    <GameConsole />
-                  </template>
-                  <template #card-title>
-                    All your favorite titles.
-                  </template>
-                  <div class="joke-wrapper">
-                    <div class="body-text">
-                      Bloodborne coming Soon™*
-                    </div>
-                    <div class="joke-footnote">
-                      *RPCSX does not provide estimates or compatibility for any title.
-                    </div>
-                  </div>
-                  </ContentCard>
-                  <ContentCard>
-                    <template #card-icon>
-                      <Linux />
-                    </template>
-                    <template #card-title>
-                      Currently Linux only.
-                    </template>
-                    WSL on Windows is being discussed.
-                  </ContentCard> -->
-              </div>
+              <!-- todo put something here if necessary -->
+              <div class="column-content" />
             </div>
             <div id="right-column" class="column">
               <div class="top-right">
